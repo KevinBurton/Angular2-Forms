90e81a2b5d1794f8a991e36cc3ded9780aba81bf
Kevin Burton <rkevinburton@charter.net>
2016-12-28 04:40:33 -0600

    Added file based URL for languages

--- src/app/services/form-poster.service.ts
+++ src/app/services/form-poster.service.ts
@@ -3,36 +3,40 @@ import { Http, Request, Headers, RequestOptions, Response } from '@angular/http'
 import 'rxjs/Rx';
 import { Observable } from 'rxjs/Observable';
 import { Employee } from '../models/employee.model';
-
+const URL_LANGUAGES = 'app/resources/languages.json';
 @Injectable()
 export class FormPoster {
-    constructor(private http: Http) {
+    constructor(private _http: Http) {
 
     }
-    private extractLanguages(res:Response) {
+    private _extractLanguages(res:Response) {
         let body = res.json();
         return body.data || {};
     }
-    private extractData(res: Response) {
+    private _extractData(res: Response) {
         let body = res.json();
         return body.fields || {};
     }
-    private handleError(error: any) {
+    private _handleError(error: any) {
         console.error('post error: ', error);
         return Observable.throw(error.statusText);
     }
     getLanguages(): Observable<any> {
-        return this.http.get('http://localhost:3100/get-languages')
-                        .map(this.extractLanguages)
-                        .catch(this.handleError);
+        return this._http.get(URL_LANGUAGES)
+                   .map((response: Response) => response.json())
+                   .catch(this._handleError);
+    
+        // return this.http.get('http://localhost:3100/get-languages')
+        //                 .map(this._extractLanguages)
+        //                 .catch(this._handleError);
     }
     postEmployeeForm(employee: Employee): Observable<any> {
         let body = JSON.stringify(employee);
         let headers = new Headers({ 'Content-Type': 'application/json' });
         let options = new RequestOptions({ headers: headers })
 
-        return this.http.post('http://localhost:3100/postemployee', body, options)
-                        .map(this.extractData)
-                        .catch(this.handleError);
+        return this._http.post('http://localhost:3100/postemployee', body, options)
+                        .map(this._extractData)
+                        .catch(this._handleError);
     }
 }
